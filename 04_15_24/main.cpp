9927f9498d4ce4676ab90f6cbc5d67a9d65a2796
Angela <avenable11@ivytech.edu>
2024-04-15 19:34:17 -0400

    template function + vector

---
+++ 04_15_24/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <random>
+#include <vector>
+#include "clock.h"
+
+template <class t>
+t magicTemplate(t val1, t val2);
+
+int main()
+{
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> distribution(1, 100);
+    int x = distribution(generator);
+    int y = distribution(generator);
+    int z = magicTemplate<int>(x, y);
+
+    std::uniform_real_distribution<double> distributionf(1, 50);
+    double xf = distributionf(generator);
+    double yf = distributionf(generator);
+    double zf = magicTemplate<double>(xf, yf);
+
+    std::cout << "z = " << z << std::endl;
+    std::cout << "zf = " << zf << std::endl;
+    clockType a(distribution(generator), distribution(generator), distribution(generator), TWELVE, PM);
+    clockType b(distribution(generator), distribution(generator), distribution(generator), TWENTYFOUR);
+    clockType c = magicTemplate<clockType>(a, b);
+    std::cout << "c = " << c << std::endl;
+
+    std::vector<clockType> clocks;
+    clocks.push_back(a);
+    clocks.push_back(clockType(1, 1, 1));
+    return 0;
+}
+
+template <class t>
+t magicTemplate(t val1, t val2)
+{
+    t answer = val1 + val2;
+    /*  if (val1 >= val2)
+         answer = answer * val1;
+     else
+         answer = answer * val2; */
+    return answer;
+}
